6c05be3fb8a8da662403fbd29e925ffc47415ce4
FelipeLemir04 <felipe.lemir04@gmail.com>
2024-03-20 12:08:53 -0300

    Global Pending UI

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css?url";
@@ -36,6 +37,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -102,7 +104,10 @@ export default function App() {
 
           </nav>
         </div>
-        <div id="detail">
+        <div className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+          id="detail">
           <Outlet />
         </div>
 
